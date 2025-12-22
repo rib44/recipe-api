0231ca9317dedbe7a94d33b7cb83c4837e76c581
Aryan Kumar <aryankr730@gmail.com>
2025-12-22 19:39:21 +0530

    Added ingredients feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -315,7 +315,7 @@ class PrivateRecipeAPITests(TestCase):
         for ingredient in payload['ingredients']:
             exists = recipe.ingredients.filter(
                 name=ingredient['name'],
-                user = self.user,
+                user=self.user,
             ).exists()
             self.assertTrue(exists)
 
